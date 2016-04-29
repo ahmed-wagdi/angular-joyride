7fbd64e55570d14990d4d854271ca7be835c2483
ahmed wagdi <ahmedwagdi@ahmeds-MacBook-Pro.local>
2016-04-29 23:58:54 +0200

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', ['angular-joyride']);
 
 app.controller('mainController', ['$scope', 'joyrideService', function($scope, joyrideService){
 	var joyride = joyrideService;
-
+	console.log(joyrideService);
 	joyride.start = true;
 
 	joyride.steps = [
