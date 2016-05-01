af09de4d40aa0e42d1d6dd36c4bc460182809506
ahmed wagdi <ahmedwagdi@ahmeds-MacBook-Pro.local>
2016-05-01 21:49:18 +0200

    edit

--- app.js
+++ app.js
@@ -1,3 +1,46 @@
+/*
+	By Osvaldas Valutis, www.osvaldas.info
+	Available for use under the MIT License
+*/
+
+;( function ( document, window, index )
+{
+	var s = document.body || document.documentElement, s = s.style, prefixAnimation = '', prefixTransition = '';
+
+	if( s.WebkitAnimation == '' )	prefixAnimation	 = '-webkit-';
+	if( s.MozAnimation == '' )		prefixAnimation	 = '-moz-';
+	if( s.OAnimation == '' )		prefixAnimation	 = '-o-';
+
+	if( s.WebkitTransition == '' )	prefixTransition = '-webkit-';
+	if( s.MozTransition == '' )		prefixTransition = '-moz-';
+	if( s.OTransition == '' )		prefixTransition = '-o-';
+
+	Object.prototype.onCSSAnimationEnd = function( callback )
+	{
+		var runOnce = function( e ){ callback(); e.target.removeEventListener( e.type, runOnce ); };
+		this.addEventListener( 'webkitAnimationEnd', runOnce );
+		this.addEventListener( 'mozAnimationEnd', runOnce );
+		this.addEventListener( 'oAnimationEnd', runOnce );
+		this.addEventListener( 'oanimationend', runOnce );
+		this.addEventListener( 'animationend', runOnce );
+		if( ( prefixAnimation == '' && !( 'animation' in s ) ) || getComputedStyle( this )[ prefixAnimation + 'animation-duration' ] == '0s' ) callback();
+		return this;
+	};
+
+	Object.prototype.onCSSTransitionEnd = function( callback )
+	{
+		var runOnce = function( e ){ callback(); e.target.removeEventListener( e.type, runOnce ); };
+		this.addEventListener( 'webkitTransitionEnd', runOnce );
+		this.addEventListener( 'mozTransitionEnd', runOnce );
+		this.addEventListener( 'oTransitionEnd', runOnce );
+		this.addEventListener( 'transitionend', runOnce );
+		this.addEventListener( 'transitionend', runOnce );
+		if( ( prefixTransition == '' && !( 'transition' in s ) ) || getComputedStyle( this )[ prefixTransition + 'transition-duration' ] == '0s' ) callback();
+		return this;
+	};
+}( document, window, 0 ));
+
+
 var app = angular.module('myApp', ['angular-joyride', 'vesparny.fancyModal']);
 
 app.controller('mainController', ['$scope', 'joyrideService', '$fancyModal', function($scope, joyrideService, $fancyModal){
@@ -14,8 +57,12 @@ app.controller('mainController', ['$scope', 'joyrideService', '$fancyModal', fun
         	overlay: false
         });
         modal.opened.then(function() {
-		  resume();
-		  console.log(true);
+        	item.onCSSAnimationEnd( function()
+			{
+			    resume();
+				console.log(true);
+			});
+		  
 		});
     };
 
