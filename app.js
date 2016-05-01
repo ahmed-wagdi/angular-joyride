beec11482c4752f27bcdd227b7fa4eb5c7f2f345
ahmed wagdi <ahmedwagdi@ahmeds-MacBook-Pro.local>
2016-05-01 21:54:59 +0200

    edit

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.controller('mainController', ['$scope', 'joyrideService', '$fancyModal', fun
         	overlay: false
         });
         modal.opened.then(function() {
-        	document.queryselector('fancymodal-content').onCSSAnimationEnd( function()
+        	document.querySelector('fancymodal-content').onCSSAnimationEnd( function()
 			{
 			    resume();
 				console.log(true);
